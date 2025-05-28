d8d7ec44e18364e789fc6531ce92a25177a7eb5d
NikitaAksenov <naksenov96@gmail.com>
2025-05-28 20:56:22 +0300

    Drafted db and commands structure

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
 	fmt.Println("expenses")
+
+	if len(os.Args) < 2 {
+		return
+	}
+
+	dbPath := "./db/expenses.db"
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	switch os.Args[1] {
+	case "add":
+	case "upd":
+	case "list":
+	case "del":
+	}
 }
