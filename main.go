d8dd10b62f3e35f49512ee72bff5713c11bc092a
NikitaAksenov <naksenov96@gmail.com>
2025-06-01 09:55:08 +0300

    Added some filters to 'list' command

--- main.go
+++ main.go
@@ -211,7 +211,129 @@ func main() {
 	case "list":
 		fmt.Println("Listing expense entries")
 
-		rows, err := db.Query("select * from expenses order by Date")
+		fs := flag.NewFlagSet("list", flag.ExitOnError)
+
+		var dateFlag bool
+		dateFlagDesc := "List expenses for date"
+		fs.BoolVar(&dateFlag, "date", false, dateFlagDesc)
+		fs.BoolVar(&dateFlag, "d", false, dateFlagDesc)
+
+		var dateRangeFlag bool
+		dateRangeFlagDesc := "List expenses for date range"
+		fs.BoolVar(&dateRangeFlag, "daterange", false, dateRangeFlagDesc)
+		fs.BoolVar(&dateRangeFlag, "dr", false, dateRangeFlagDesc)
+
+		var categoryFlag bool
+		categoryFlagDesc := "List expenses for category"
+		fs.BoolVar(&categoryFlag, "category", false, categoryFlagDesc)
+		fs.BoolVar(&categoryFlag, "c", false, categoryFlagDesc)
+
+		var categoriesFlag bool
+		categoriesFlagDesc := "List expenses for categories"
+		fs.BoolVar(&categoriesFlag, "categories", false, categoriesFlagDesc)
+		fs.BoolVar(&categoriesFlag, "cs", false, categoriesFlagDesc)
+
+		var limitFlag int
+		limitFlagDesc := "Limit expenses count"
+		fs.IntVar(&limitFlag, "limit", 100, limitFlagDesc)
+		fs.IntVar(&limitFlag, "l", 100, limitFlagDesc)
+
+		fs.Parse(os.Args[2:])
+
+		if dateFlag && dateRangeFlag {
+			fmt.Println("Can't have `date` and `daterange` flags both present")
+			return
+		}
+
+		if categoryFlag && categoriesFlag {
+			fmt.Println("Can't have `category` and `categories` flags both present")
+			return
+		}
+
+		if limitFlag <= 0 {
+			fmt.Println("Limit flag must be > 0, but entered", limitFlag)
+			return
+		}
+
+		var whereArgs []string
+
+		if dateFlag {
+			fmt.Print("Date:     ")
+			dateStr, _ := reader.ReadString('\n')
+			dateStr = strings.TrimSuffix(dateStr, "\r\n")
+
+			_, err := time.Parse(TimeFormatDateOnly, dateStr)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			whereArgs = append(whereArgs, fmt.Sprintf("Date = '%s'", dateStr))
+		}
+
+		if dateRangeFlag {
+			{
+				fmt.Print("Date from:     ")
+				dateStr, _ := reader.ReadString('\n')
+				dateStr = strings.TrimSuffix(dateStr, "\r\n")
+
+				_, err := time.Parse(TimeFormatDateOnly, dateStr)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+
+				whereArgs = append(whereArgs, fmt.Sprintf("Date >= '%s'", dateStr))
+			}
+			{
+				fmt.Print("Date to  :     ")
+				dateStr, _ := reader.ReadString('\n')
+				dateStr = strings.TrimSuffix(dateStr, "\r\n")
+
+				_, err := time.Parse(TimeFormatDateOnly, dateStr)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+
+				whereArgs = append(whereArgs, fmt.Sprintf("Date <= '%s'", dateStr))
+			}
+		}
+
+		if categoryFlag || categoriesFlag {
+			var categories []string
+
+			for ok := true; ok; ok = categoriesFlag {
+				fmt.Print("Category: ")
+				category, _ := reader.ReadString('\n')
+				category = strings.TrimSuffix(category, "\r\n")
+
+				if category == "" {
+					break
+				}
+
+				categories = append(categories, "'"+category+"'")
+			}
+
+			if len(categories) > 0 {
+				whereArgs = append(whereArgs, fmt.Sprintf("Category IN (%s)", strings.Join(categories, ",")))
+			}
+		}
+
+		// Make query
+		query := "SELECT * FROM expenses"
+
+		if len(whereArgs) > 0 {
+			whereStmt := strings.Join(whereArgs, " AND ")
+			query += "\nWHERE " + whereStmt
+		}
+
+		query += "\nORDER BY Date"
+		query += fmt.Sprintf("\nLIMIT %d", limitFlag)
+
+		fmt.Println(query)
+
+		rows, err := db.Query(query)
 		if err != nil {
 			fmt.Println(err.Error())
 			return
